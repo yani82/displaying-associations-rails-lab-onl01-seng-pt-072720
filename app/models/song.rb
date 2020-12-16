443492b572bf5edb29c52ac069de49c52a405733
yani82 <yani.iman1@gmail.com>
2020-12-16 00:35:04 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name 
+    self.artist.name 
+  end 
+
+  def artist_and_title
+    artist_name + " - " + self.title 
+  end 
 end
